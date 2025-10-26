eacded075cf2bb7e8d2380b31e45ef7a985684d7
bhunith058 <bhunithkumarbhuvan058@gmail.com>
2025-10-26 16:58:18 +0530

    Initial commit: Added music recommendation system project

---
+++ name=input().py
@@ -0,0 +1,8 @@
+name=input()
+age=int(input())
+cgpa=float(input())
+grade=input()[0]
+print("Name: {}".format(name))
+print("Age: {}".format(age))
+print("CGPA: {:.2f}".format(cgpa))
+print("Grade: {}".format(grade))
